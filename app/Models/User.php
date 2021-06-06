f2c4b9953a78d3f19fe9b1761736ca20538e9ba8
HelfanzaNanda <elvannnd@gmail.com>
2021-06-06 12:16:03 +0700

    api token

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [ 'name', 'email', 'password', 'role' ];
+    protected $fillable = [ 'name', 'email', 'password', 'role', 'api_token' ];
 
     /**
      * The attributes that should be hidden for arrays.
